c134bd9845cc41afc173c861e7fd2c410e56bb2b
tpepper0408 <tpepper04@gmail.com>
2020-12-02 07:53:46 +0000

    Changing file reader tests to look at non-existent day so it doesn't conflict with other tests

--- src/test/kotlin/util/UtilTests.kt
+++ src/test/kotlin/util/UtilTests.kt
@@ -9,13 +9,13 @@ class UtilTests {
 
     @Test
     fun testReadInputAsString() {
-        val testInputAsString = InputReader.getInputAsString(1)
-        assertThat(testInputAsString, `is`("1721\n979\n366\n299\n675\n1456\n"))
+        val testInputAsString = InputReader.getInputAsString(26)
+        assertThat(testInputAsString, `is`("1539\n1914\n1866\n1407\n1706\n1456\n"))
     }
 
     @Test
     fun testReadInputAsList() {
-        val testInputAsList = InputReader.getInputAsList(1)
-        assertThat(testInputAsList, contains("1721", "979", "366", "299", "675", "1456"))
+        val testInputAsList = InputReader.getInputAsList(26)
+        assertThat(testInputAsList, contains("1539", "1914", "1866", "1407", "1706", "1456"))
     }
 }
