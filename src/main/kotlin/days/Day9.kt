6110f299d3e1a92d36a77017b1c0a9eb57281163
tpepper0408 <tpepper04@gmail.com>
2020-12-08 18:47:00 +0000

    DAY9: Setup

---
+++ src/main/kotlin/days/Day9.kt
@@ -0,0 +1,11 @@
+package days
+
+class Day9 : Day<Int>(9) {
+    override fun partOne(): Int {
+        return 0
+    }
+
+    override fun partTwo(): Int {
+        return 0
+    }
+}
---
+++ src/test/kotlin/days/Day9Test.kt
@@ -0,0 +1,19 @@
+package days
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class Day9Test {
+    val day9 = Day9()
+
+    @Test
+    fun partOne() {
+        assertEquals(0, day9.partOne())
+    }
+
+    @Test
+    fun partTwo() {
+        assertEquals(0, day9.partTwo())
+    }
+}
